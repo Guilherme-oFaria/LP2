db9d1264ecd966bf907a1737b92505408d6c714c
Guilherme-oFaria <79918061+Guilherme-oFaria@users.noreply.github.com>
2021-03-24 22:24:26 -0300

    Add files via upload

---
+++ Atividade3/Atividade3/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Atividade3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblA = new System.Windows.Forms.Label();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.btnConv = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(115, 101);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(44, 13);
+            this.lblA.TabIndex = 0;
+            this.lblA.Text = "Lado A:";
+           
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(296, 101);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(44, 13);
+            this.lblB.TabIndex = 1;
+            this.lblB.Text = "Lado B:";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(472, 101);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(44, 13);
+            this.lblC.TabIndex = 2;
+            this.lblC.Text = "Lado C:";
+        
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(162, 98);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 3;
+        
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(343, 98);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 4;
+ 
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(519, 98);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(100, 20);
+            this.txtC.TabIndex = 5;
+        
+            // btnConv
+            // 
+            this.btnConv.Location = new System.Drawing.Point(299, 181);
+            this.btnConv.Name = "btnConv";
+            this.btnConv.Size = new System.Drawing.Size(75, 23);
+            this.btnConv.TabIndex = 6;
+            this.btnConv.Text = "Converter";
+            this.btnConv.UseVisualStyleBackColor = true;
+            this.btnConv.Click += new System.EventHandler(this.btnConv_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(397, 181);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(762, 414);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnConv);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button btnConv;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
